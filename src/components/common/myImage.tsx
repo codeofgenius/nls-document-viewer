8c904a595cde6d64fe6bf9e94510b26e7c9c7c5d
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2025-09-03 07:22:21 +0000

    fix myImage

--- src/components/common/myImage.tsx
+++ src/components/common/myImage.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import Image, { type StaticImageData } from 'next/image';
 import { twMerge } from 'tailwind-merge';
 
+import noImage from '@/assets/image/noimage.png';
 import { type MyImageProps } from '@/types/common';
 
 export function MyImage({
@@ -15,13 +16,12 @@ export function MyImage({
   width,
   height,
 }: MyImageProps) {
-  // const [imageSrc, setImageSrc] = useState<string | StaticImageData>(src);
   const [imageSrc, setImageSrc] = useState<string | StaticImageData>(src);
   const [cn, setCn] = useState(className);
 
   function handleImageError() {
-    // setImageSrc(noImage);
-    setImageSrc(src);
+    setImageSrc(noImage);
+    // setImageSrc(src);
     setCn((cn) => twMerge(cn, errorClassName));
   }
 
